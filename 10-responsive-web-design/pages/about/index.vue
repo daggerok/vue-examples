69e69fdf62704ab401c1fcf83f67c5b4c674b5ad
Maksim Kostromin <daggerok@gmail.com>
2017-08-25 06:19:05 +0300

    implement about

--- 10-responsive-web-design/pages/about/index.vue
+++ 10-responsive-web-design/pages/about/index.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="container">
-    <h1>About page</h1>
-  </div>
+  <section id="about">
+    <article id="main">
+      <div class="container">
+        <h1>About me</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur consequatur dolor esse expedita explicabo ipsam, libero modi nemo nobis quam quasi quos reiciendis unde. Consectetur dolor perferendis quibusdam sit voluptatibus.
+        </p>
+        <p class="dark">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis ea eveniet magnam nihil pariatur quibusdam rem voluptatibus. Asperiores cum et ipsa laborum minus modi nobis omnis optio placeat, praesentium velit!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, nisi, officiis! Ad, amet aspernatur dicta dignissimos officia quo quod voluptatem. Asperiores blanditiis debitis delectus natus pariatur porro recusandae soluta ut.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta ex expedita ipsam nisi nulla reiciendis repellendus sint tenetur velit. Minima, molestias, porro. Aliquid assumenda deleniti ducimus exercitationem labore laboriosam, quasi!
+        </p>
+      </div>
+    </article>
+    <aside id="sidebar">
+      <div class="container">
+        <h1>Team</h1>
+        <p class="dark">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur autem dignissimos doloribus eius, fugiat incidunt ipsam iusto labore nisi odio perspiciatis qui quisquam quos rerum sit, ut voluptas voluptatum?
+        </p>
+      </div>
+    </aside>
+  </section>
 </template>
 
 <script>
@@ -9,5 +29,3 @@
     name: 'about',
   };
 </script>
-
-<style scoped=""></style>
