2dc2217606fd809facc45eabe076be7cbbe4d35b
Maksim Kostromin <daggerok@gmail.com>
2018-07-21 18:49:53 +0300

    Delete redeploy.sh

--- .mvn/redeploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# bash mvnw clean war:war -DskipTests; bash .mvn/redeploy.sh
-container=vue-examples_maven-vue-examples-app_1
-docker cp ./target/*.war ${container}:/home/jboss-eap-7.1/jboss-eap-7.1/standalone/deployments/app.war
-
