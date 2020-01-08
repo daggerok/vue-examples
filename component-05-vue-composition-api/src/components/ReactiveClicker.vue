dfe60e992f64df5b6c3635076f6ec53c2cd2819e
Maksim Kostromin <daggerok@gmail.com>
2020-01-08 03:08:36 +0200

    epic (C) whatthecommit.com

--- component-05-vue-composition-api/src/components/ReactiveClicker.vue
+++ component-05-vue-composition-api/src/components/ReactiveClicker.vue
@@ -1,5 +1,22 @@
 <template>
-  <button @click="this.increment">reactive: {{ state.counter }}</button>
+  <div class="container">
+    <div class="item">
+      <button @click="this.increment">
+        reactive increment
+        <span class="bold">{{ state.counter }}</span>
+        counter of
+        <span class="bold">{{ state.totalClicks }}</span> total clicks
+      </button>
+    </div>
+    <div class="item">
+      <button @click="this.decrement">
+        reactive decrement
+        <span class="bold">{{ state.counter }}</span>
+        counter of
+        <span class="bold">{{ state.totalClicks }}</span> total clicks
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,16 +26,37 @@
     setup() {
       const state = reactive({
         counter: 0,
+        totalClicks: 0,
       });
-      const increment = () => state.counter += 1;
+      const increment = () => {
+        state.counter += 1;
+        state.totalClicks += 1;
+      };
+      const decrement = () => {
+        state.counter -= 1;
+        state.totalClicks += 1;
+      };
       return {
         state,
         increment,
+        decrement,
       };
     },
   };
 </script>
 
 <style scoped>
-
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+  .item {
+    margin: auto;
+  }
+  .item button {
+    font-size: 2em;
+  }
+  .bold {
+    font-weight: bold;
+  }
 </style>
